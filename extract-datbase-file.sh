0cd853d1eea01773d1f28c33ba6cf16f800f22cf
Ikaro Silva <ikarosilva@yahoo.com>
2015-11-19 13:18:22 -0500

    Delete extract-datbase-file.sh
    Querying done via sqlite python driver.

--- extract-datbase-file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Exporting database info from Banshee..."
-sqlite3 ~/.config/banshee-1/banshee.db .dump CoreTracks | grep INSERT | sed "s/INSERT INTO \"CoreTracks\" VALUES(//;s/);//" > banshee-tracks
-
-echo "Finished exporting database info from Banshee. Created file banshee-tracks.s"
-
